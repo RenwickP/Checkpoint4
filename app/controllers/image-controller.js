375ce5f0b7403eaf6293274ce3b218bbc65d2533
renwickp <peter2renwick@gmail.com>
2019-12-06 14:49:59 -0700

    Test

--- app/controllers/image-controller.js
+++ app/controllers/image-controller.js
@@ -1,5 +1,14 @@
 import ImageService from "../services/image-service.js";
+import store from "../store.js";
+import imageService from "../services/image-service.js";
+
+function _drawImage() {}
 
 //TODO Create methods for constructor, and rendering the image to the page
 //      (you may wish to set it as a background image)
-export default class ImageController {}
+export default class ImageController {
+  constructor() {
+    imageService.getPic();
+    // store.subscribe();
+  }
+}
--- app/services/image-service.js
+++ app/services/image-service.js
@@ -1,3 +1,5 @@
+import store from "../store";
+
 // @ts-ignore
 const imgApi = axios.create({
   baseURL: "//bcw-sandbox.herokuapp.com/api/images",
@@ -5,7 +7,14 @@ const imgApi = axios.create({
 });
 
 //TODO create methods to retrieve data trigger the update window when it is complete
-class ImageService {}
+class ImageService {
+  async getPic() {
+    let pic = await imgApi.get();
+    store.commit("image", pic);
+    console.log(pic);
+    console.log("from commit", pic);
+  }
+}
 
 const imageService = new ImageService();
 export default imageService;
